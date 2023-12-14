c701da54f38d0c71c944fb0745afaadb3c991a72
Alex Matsoukas <amatsoukas@olin.edu>
2023-12-14 13:32:19 -0500

    tack build, install, log from gitignore due to gh-pages interference

---
+++ build/orbslam3/CMakeFiles/stereo-inertial.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for stereo-inertial.
+# This may be replaced when dependencies are built.
---
+++ build/orbslam3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/amatsoukas/ros2_ws/src/orbslam3_ros2/src/orbslam3_ros2
+# Build directory: /home/amatsoukas/ros2_ws/src/orbslam3_ros2/build/orbslam3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
